e0344d9dadcf62bda62686c0ca14fdc522a6c170
gjohl <gurpreetjohl@gmail.com>
2020-01-05 11:36:42 +0000

    Add long bias and short bias signals

---
+++ alphadog/signals/bias.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+from ..framework.constants import AVG_FORECAST
+
+
+def long_bias_signal(df):
+    """
+    A signal which is a constant long signal for all of time.
+
+    The value is set to the average forecast value for all timestamps.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame to run long bias signal on.
+
+    Returns
+    -------
+    long_bias_df: pd.DataFrame
+        Columnwise long bias signal.
+        Returns the same columns as the input.
+    """
+    long_bias_df = df.copy()
+
+    # Set all values to AVG_FORECAST while preserving dtypes
+    for col in long_bias_df.columns:
+        long_bias_df[col].values[:] = AVG_FORECAST
+
+    return long_bias_df
+
+
+def short_bias_signal(df):
+    """
+    A signal which is a constant short signal for all of time.
+
+    The value is set to the negative of the average forecast value for all timestamps.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame to run short bias signal on.
+
+    Returns
+    -------
+    short_bias_df: pd.DataFrame
+        Columnwise short bias signal.
+        Returns the same columns as the input.
+    """
+    short_bias_df = df.copy()
+
+    # Set all values to negative AVG_FORECAST while preserving dtypes
+    for col in short_bias_df.columns:
+        short_bias_df[col].values[:] = -AVG_FORECAST
+
+    return short_bias_df
---
+++ tests/unit/signals/test_bias.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from alphadog.signals.bias import long_bias_signal, short_bias_signal
+
+
+class TestLongBias:
+
+    def test_single_column_df(self, mock_price):
+        """Test a single column input returns the expected output."""
+        actual = long_bias_signal(mock_price)
+
+        index = pd.bdate_range('2019-01-01', periods=20, name='timestamp')
+        expected = pd.DataFrame(data=[10] * 20, columns=['price'], index=index, dtype=float)
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+    def test_multiple_column_df(self, mock_ohlcv):
+        """Test that multi-column inputs are calculated columnwise."""
+        actual = long_bias_signal(mock_ohlcv)
+
+        vals = [10] * 20
+        index = pd.bdate_range('2019-01-01', periods=20, name='timestamp')
+        expected = pd.DataFrame({
+            'open': vals,
+            'high': vals,
+            'low': vals,
+            'close': vals,
+            'volume': vals},
+            index=index,
+            dtype=float)
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+
+class TestShortBias:
+
+    def test_single_column_df(self, mock_price):
+        """Test a single column input returns the expected output."""
+        actual = short_bias_signal(mock_price)
+
+        index = pd.bdate_range('2019-01-01', periods=20, name='timestamp')
+        expected = pd.DataFrame(data=[-10] * 20, columns=['price'], index=index, dtype=float)
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+    def test_multiple_column_df(self, mock_ohlcv):
+        """Test that multi-column inputs are calculated columnwise."""
+        actual = short_bias_signal(mock_ohlcv)
+
+        vals = [-10] * 20
+        index = pd.bdate_range('2019-01-01', periods=20, name='timestamp')
+        expected = pd.DataFrame({
+            'open': vals,
+            'high': vals,
+            'low': vals,
+            'close': vals,
+            'volume': vals},
+            index=index,
+            dtype=float)
+
+        pd.testing.assert_frame_equal(actual, expected)
